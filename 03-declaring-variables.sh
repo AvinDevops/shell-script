4def2d44b22e43cb2994a1594f83d405b9ee98d4
AvinDevps <avinashbandela73@gmail.com>
2024-07-23 21:46:32 +0530

    declaring variables

--- 03-declaring-variables.sh
+++ 03-declaring-variables.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-PERSON1=Ramesh
-PERSON2=Suresh
+PERSON1=RAMESH
+PERSON2=SURESH
 
 echo "$PERSON1 :: Hi $PERSON2, How are you?"
 echo "$PERSON2 :: Hi $PERSON1, I am fine. How are you?"
