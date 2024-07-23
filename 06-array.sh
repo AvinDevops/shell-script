9ae0893755071a9117097fac1f3e73bbf0e18083
AvinDevps <avinashbandela73@gmail.com>
2024-07-23 22:12:12 +0530

    using array

---
+++ 06-array.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+MOVIES = ("Civil War", "Infinity War", "End Game")
+# list always starts with index zero (0)
+#size of above array is 3
+#index are 0,1,2
+
+echo "First Movie is : ${MOVIES[0]}"
+echo "Second Movie is : ${MOVIES[1]}"
+echo "Third Movie is : ${Movies[2]}"
+echo "Show all : ${Movies[@]}"
