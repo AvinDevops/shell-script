b33680056cd0d1007e5e161dc6b7667094cf213e
AvinDevps <avinashbandela73@gmail.com>
2024-07-31 18:03:52 +0530

    checking error with set -e

--- 17-set-e.sh
+++ 17-set-e.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
 set -e
 
 error_handler(){
-    echo "error came at $1:$2"
+    echo "error came at $R $1:$2 $N"
 }
 
 trap 'error_handler ${LINENO} "$BASH_COMMAND"' ERR
@@ -12,10 +16,10 @@ USERID=$(id -u)
 
 if [ $USERID -ne 0 ]
 then
-    echo "please run this script with root access"
-    exit 1
+    echo -e "$R please run this script with root access $N"
+    #exit 1
 else
-    echo "you are super user"
+    echo "$G you are super user $N"
 fi
 
 dnf install mysqll -y
