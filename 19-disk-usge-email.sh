56d8dd95e0afcb04ff25b7e635debb59407a552d
AvinDevps <avinashbandela73@gmail.com>
2024-08-01 20:26:42 +0530

    checking disk useage and sending email

--- 19-disk-usge-email.sh
+++ 19-disk-usge-email.sh
@@ -1,9 +1,14 @@
 DISK_USEAGE=$(df -hT | grep xfs)
-DISK_THRESHOLD=75
+DISK_THRESHOLD=7
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
 
 while IFS= read -r line
 do
     USEAGE=$(echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1)
     FOLDER=$(echo $line | awk -F " " '{print $NF}')
-    echo "$FOLDER useage is more than $DISK_THRESHOLD, current useage:$USEAGE"
+    echo -e "$Y $FOLDER $N useage is more than $R $DISK_THRESHOLD $N, current useage:$G $USEAGE $N"
 done <<< $DISK_USEAGE
