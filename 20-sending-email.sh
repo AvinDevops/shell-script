9c08fb080a0f22b56358f7e539ab9c1889c22b21
AvinDevps <avinashbandela73@gmail.com>
2024-08-01 21:17:01 +0530

    Sending mail

--- 20-sending-email.sh
+++ 20-sending-email.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-echo "This is a test mail & Date $(date)" | mail -s "message" info@joindevops.com
+echo "This is a test mail & Date $(date)" | mail -s "message" avinashbandela25@gmail.com
 
 # echo "body of the mail" | mail -s "subject" tomailaddress
